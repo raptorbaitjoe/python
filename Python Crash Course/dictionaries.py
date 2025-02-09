1bc1faef989593660f113f896fb24621a6b5acb4
raptorbaitjoe <josephb.1993@gmail.com>
2025-02-09 16:54:00 +0000

    Create dictionaries.py

---
+++ Python Crash Course/dictionaries.py
@@ -0,0 +1,8 @@
+scott = {'thunderbird': 1, 'colour': 'blue'}
+virgil = {'thunderbird': 2, 'colour': 'yellow'}
+alan = {'thunderbird': 3, 'colour': 'white'}
+gordon = {'thunderbird': 4, 'colour': 'orange'}
+
+print(scott['thunderbird'])
+print(virgil['thunderbird'])
+print(scott['colour'])
