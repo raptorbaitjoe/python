42f7bd408b32e2e891a8d34e8047febb054cc7c0
Joe <josephb.1993@gmail.com>
2025-03-26 16:49:41 +0000

    Create 1 - Understanding Cloud Computing.py

---
+++ Data Engineer in Python/1 - Understanding Cloud Computing.py
@@ -0,0 +1,64 @@
+### Introduction to Cloud Computing
+
+# At its core, cloud computing is the delivery of technology services—like computing power, storage, databases, and networking—over the internet, 
+# with pay-as-you-go pricing. Instead of owning and maintaining physical servers or data centers, companies can rent access to these services from 
+# cloud providers like AWS, Microsoft Azure, or Google Cloud Platform. 
+
+# Physical Servers
+# - Traffic increases require more servers
+# - Buying or renting servers is expensive
+# - If traffic drops, you pay for excess capacity
+# - More control and security
+
+# Cloud Computing
+# - No owning physical servers
+# - Website run on cloud servers
+# - Scale up or down instantly based on demand
+# - Only pay for what you use
+# Security and cost can be an issue
+
+
+### The Power of the Cloud
+
+# Three main cloud services: Compute, Storage and Databases
+# Compute is the brain that processes workloads
+# Storage saves files and data
+# Databases store data for easy retireval, management and analysis
+# Businesses can use all three in combination for powerful cloud solutions
+
+# Virtualisation
+# Splits one physical server into multiple virtual servers, each acting like it's own independent machine
+# Means better resources efficiency - no wasted space
+# Lower costs due to less physcial machines
+
+# Scalability
+# Adjust resources based on demand
+# Vertical Scaling - Upgrades and existing machines power
+# Horizontal scaling - adds more machines to share workload 
+
+# Pay-as-you-go pricing
+# Scale up for high demand
+# Scale back in slower traffic
+# On-premise solutions more cost-effective if traffic is relatively stable
+
+# Speed
+# Enables quick launch of services and applications
+# Performance and growth are optimsed with the cloud
+# On-premise can't compete with constant cloud updates
+# Cloud allows quick deploy of services and resources globally
+
+# Reliability
+# Cloud providers replicates data across multiple data centres
+# Less downtimes
+# Fewr outages
+# More piece of mind
+
+# Security
+# Very important when trusting thrid party with your data
+# Some industries have strict regulations now met by many cloud providers
+
+
+
+
+
+
