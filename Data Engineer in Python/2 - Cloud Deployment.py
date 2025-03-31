e60a51e7aea1e2ea3ad1eece3f72cff911908a1d
Joe <josephb.1993@gmail.com>
2025-03-31 17:17:41 +0100

    Create 2 - Cloud Deployment.py

---
+++ Data Engineer in Python/2 - Cloud Deployment.py
@@ -0,0 +1,43 @@
+### Cloud Deployment Models
+
+# Important decision in cloud adoption
+# Depends on how much control needed over cloud environment
+# Three main types: private, public & hybrid
+
+## Private CLoud
+# Designated for exclusive use by its tenants
+# Accessed by a network link, special access need set up by IT
+# Appealing because gives direct control of resources (i.e tech model) and data storage
+# Costs more upfront investment due to structure design, hardware purchase etc.
+# Uses virtualisation for on-demand compute resources and can be off-premises
+
+## Public Cloud
+# CLoud infrastructure is shared and open for use by the general public
+# Infrastructure owned and managed by cloud service providers
+# Example: AWS
+# Public clouds are internet accessible
+# Allows quick start with minimal investment, no need to host or commit to infrastructure
+# Can pick what services are needed from provider
+# Easier to scale as can by more capacity
+# No access to data centre and hardware, data centre locations are secret for security reasons
+
+## Hybrid Cloud
+# When and organisation uses two or more distinct models
+# Models interact with each other via network link and can share data and services
+# Could store sensitive data on a private cloud while using application from public cloud (i.e. BI tool)
+# Useful in case of Cloudbursting (private cloud overwhelmed by demand, traffic moved to public cloud instance to avoid disruption of service)
+
+## Other deployment models
+
+# Multicloud
+# combination of different cloud provider services
+# Flexibility on pricing plans and service offerings
+# No reliance on one vendor
+
+# Community model
+# Cloud infrastructure shared by specific community for specific use
+# Shared interest or concern e.g. security, jurisdiction, mission etc.
+# Easier collaboration and sharing of data
+# Infrastructure can be managed and hosted internally or externally
+
+
