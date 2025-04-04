78994ed08ac19ceb280a6ab6346c94f93251ce12
Joe <josephb.1993@gmail.com>
2025-04-04 17:08:13 +0100

    Update 2 - Cloud Deployment.py

--- Data Engineer in Python/2 - Cloud Deployment.py
+++ Data Engineer in Python/2 - Cloud Deployment.py
@@ -41,3 +41,24 @@
 # Infrastructure can be managed and hosted internally or externally
 
 
+### Regulations on the cloud
+
+# Local regulations can affect cloud computing
+# Having widespread data centres reduces latency
+# Latency - The greater the distance between two points, the longer it will take for data to get there.
+# Country-specific regualtion on data processing and storge, for both user and data centre
+# Also regulations put on company in home country
+
+# General Data Protection Regulation (GDPR)
+# Europe-specific
+# Regulates how personal data is collected, processed and stored for EU users
+# Users must explicitly consent to data collection
+# Personal data but be encrypted, anonymised and/or pseudonymised
+# Personal data can't leave EU borders unless you can guarantee the same level of data protection
+# Fine of 20 million euros or 4% of worldwide revenue
+# Brazil, USA, Japan, Thailand and Canada have copycat regulations
+# Can influence where companies set up data centres
+
+# Personal data is any information that relates to an identified or identifiable living individual
+# Different pieces of information, which collected together can leade to identification of a particular person, also constitue personal data.
+# PII - Personally Identifiable Information
