8b30fef016b8b1036d3cef894af4f64d7079cd8a
Joe <josephb.1993@gmail.com>
2025-07-09 17:19:27 +0100

    Create files_and_exceptions.py

---
+++ Python Crash Course/files_and_exceptions.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+## Working with a file's contents
+
+path = Path('C:/Users/Joseph/Downloads/pcc_3e-main/pcc_3e-main/chapter_10/reading_from_a_file/pi_million_digits.txt')
+
+contents = path.read_text()
+
+lines = contents.splitlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line
+
+print(f"{pi_string[:52]}...")
+print(len(pi_string))
+
+birthday = input("Enter your brithday, intheforma of mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
+
+## Writing to a file
+
+contents = "I love programming. \n"
+contents += "I love creating new games. \n"
+contents += "I also love working with data. \n"
+
+pathtwo = Path('C:/Users/Joseph/OneDrive/Desktop/python_work/programming.txt')
+pathtwo.write_text(contents)
