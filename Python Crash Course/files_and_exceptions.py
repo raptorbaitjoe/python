221e9b8ec382e4a6e0a0bb0e85f2180fb54ec967
Joe <josephb.1993@gmail.com>
2025-07-09 17:29:31 +0100

    Update files_and_exceptions.py

--- Python Crash Course/files_and_exceptions.py
+++ Python Crash Course/files_and_exceptions.py
@@ -29,3 +29,29 @@ contents += "I also love working with data. \n"
 
 pathtwo = Path('C:/Users/Joseph/OneDrive/Desktop/python_work/programming.txt')
 pathtwo.write_text(contents)
+
+
+## Using Exceptions to prevent Crashes
+
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+
+
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by zero!")
+    else:
+        print(answer)
